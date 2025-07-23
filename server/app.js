0330a3e0ad8154e47c60795b5fcc401b138cca14
Natalia Linn <natalial@stanford.edu>
2025-07-22 20:58:20 -0700

    project init & basic setup

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Test route
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Ande backend is running!' });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
+module.exports = app;
