49d119a4f10493c230834e63a12ea8ce9ed70ad4
Natalia Linn <natalial@stanford.edu>
2025-07-28 23:37:59 -0700

    listen to all network interfaces

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5050;
 
-app.listen(PORT, () => {
+// get ip address command: ipconfig getifaddr en0
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
